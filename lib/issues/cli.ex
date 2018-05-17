7556429ea41a1b610d83932021aa9404746ffdce
Goh Matsumoto <urimaro@gmail.com>
2018-05-17 11:44:13 +0900

    P146 13.9 変換: 最初のn個を取り出す

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -47,6 +47,7 @@ defmodule Issues.CLI do
     |> decode_response
     |> convert_to_list_of_maps
     |> sort_into_ascending_order
+    |> Enum.take(count)
   end
 
   def decode_response({:ok, body}), do: body
