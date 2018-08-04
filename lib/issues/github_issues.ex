e6225291e32e5e56c9c7f52691a0d7d583590162
Goh Matsumoto <urimaro@gmail.com>
2018-08-04 23:04:20 +0900

    P150 Fix typo

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -15,11 +15,11 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
-    {:ok, Poison.Parsar.parse!(body)}
+    {:ok, Poison.Parser.parse!(body)}
   end
 
   def hendle_response({_, %{status_code: _, body: body}}) do
-    {:error, Poison.Parsar.parse!(body)}
+    {:error, Poison.Parser.parse!(body)}
   end
 end
 
